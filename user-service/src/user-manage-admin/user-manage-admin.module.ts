bc7b9666356b38a1d118ebaa13f4b7cc259af5ba
sielus <j.a.sielanczyk@gmail.com>
2024-04-23 22:23:43 +0200

    basic staff for admin manage

--- user-service/src/user-manage-admin/user-manage-admin.module.ts
+++ user-service/src/user-manage-admin/user-manage-admin.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserManageAdminController } from 'src/user-manage-admin/controllers/user-manage-admin.controller';
-import { UserManageAdminDaoService } from 'src/user-manage-admin/services/dao/user-manage-admin.dao.service';
-import { UserManageAdminService } from 'src/user-manage-admin/services/user-manage-admin.service';
-import { ScopeManageDaoService } from './services/scope-manage/dao/scope-manage.dao/scope-manage.dao.service';
-import { AdminManageDaoService } from './services/dao/admin-manage.dao/admin-manage.dao.service';
-import { AdminManageService } from './services/admin-manage/admin-manage/admin-manage.service';
+import { AdminManageService } from 'src/user-manage-admin/services/admin-manage/admin-manage.service';
+import { AdminManageDaoService } from 'src/user-manage-admin/services/admin-manage/dao/admin-manage.dao/admin-manage.dao.service';
+import { ScopeManageDaoService } from 'src/user-manage-admin/services/scope-manage/dao/scope-manage.dao/scope-manage.dao.service';
+
 import { ScopeManageService } from 'src/user-manage-admin/services/scope-manage/scope-manage.service';
+import { UserManageAdminService } from 'src/user-manage-admin/services/user-manage-admin.service';
 
 @Module({
   controllers: [UserManageAdminController],
-  providers: [UserManageAdminService, UserManageAdminDaoService, PrismaService, ScopeManageDaoService, AdminManageDaoService, AdminManageService, ScopeManageService],
+  providers: [
+    UserManageAdminService,
+    AdminManageDaoService,
+    PrismaService,
+    ScopeManageDaoService,
+    AdminManageDaoService,
+    AdminManageService,
+    ScopeManageService,
+  ],
 })
 export class UserManageAdminModule {}
